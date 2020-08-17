71143c669bac81629aaac2aa38f46a7cadeee21a
Richard Ivanek <richard.ivanek@gmail.com>
2020-08-17 18:48:00 +0200

    Self contained Win10 x64 build script

--- install-service.bat
+++ install-service.bat
@@ -1,4 +1,4 @@
 sc create SAApiService binPath= %~dp0sa-api
-sc failure MyService actions= restart/60000/restart/60000/""/60000 reset= 86400
-sc start MyService
-sc config MyService start=auto
+sc failure SAApiService actions= restart/60000/restart/60000/""/60000 reset= 86400
+sc start SAApiService
+sc config SAApiService start=auto

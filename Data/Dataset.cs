69062d7d9c9e526381fdaf3ef74ca837e9edacf2
Richard Ivanek <richard.ivanek@gmail.com>
2021-04-20 10:53:08 +0200

    deafult units

--- Data/Dataset.cs
+++ Data/Dataset.cs
@@ -15,6 +15,8 @@ namespace SAApi.Data
         public string Id { get; set; }
         public string[] Category { get; set; }
 
+        public string Units { get => "s^-1"; }
+
         // public string Name { get; set; }
         // public string Description { get; set; }
 
